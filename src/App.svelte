1f8b189ed256f90e55818aadb5faa607a6d82e7e
Steven Waterman <github@stevenwaterman.uk>
2020-01-22 11:43:00 +0000

    make track self-contained

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,13 @@
 <script>
-  import {
-    selectedPathStore,
-    selectedTrackAudioStore,
-    selectedTrackStore
-  } from "./trackTree.js";
-  import TrackRow from "./TrackRow.svelte";
-  import { audio } from "./audio.js";
-  import Timeline from "./Timeline.svelte";
+  import Track from "./Track.svelte";
+  import {audio} from "./audio.js";
 
-  $: paths = $selectedPathStore.map((element, idx, array) =>
-    array.slice(0, idx + 1)
-  );
+  
 </script>
 
-<style>
-  .container {
-    position: relative;
-  }
-</style>
+
 
 <main>
   <button on:click={() => audio.play(0)}>Play from Start</button>
-  <div class="container">
-    <Timeline />
-    <TrackRow path={[]} />
-    {#each paths as path}
-      <TrackRow {path} />
-    {/each}
-  </div>
+  <Track/> 
 </main>
---
+++ src/Track.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {
+    selectedPathStore,
+    selectedTrackAudioStore,
+    selectedTrackStore
+  } from "./trackTree.js";
+  import TrackRow from "./TrackRow.svelte";
+  import Timeline from "./Timeline.svelte";
+
+  $: paths = $selectedPathStore.map((element, idx, array) =>
+    array.slice(0, idx + 1)
+  );
+</script>
+
+<style>
+  .container {
+    position: relative;
+  }
+</style>
+
+<div class="container">
+  <Timeline />
+  <TrackRow path={[]} />
+  {#each paths as path}
+    <TrackRow {path} />
+  {/each}
+</div>
