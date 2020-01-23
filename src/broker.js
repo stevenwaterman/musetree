07d8c322f7a326bdca9979afb8d3d2e82ed8fbdb
Steven Waterman <github@stevenwaterman.uk>
2020-01-23 09:11:40 +0000

    add retry logic

--- src/broker.js
+++ src/broker.js
@@ -1,44 +1,65 @@
 import axios from "axios";
-import {instruments} from "./constants.js";
+import * as rax from "retry-axios";
+import { instruments } from "./constants.js";
 
-export async function request(config, prevEncoding, prevDuration){
-	const params = {
-		...config,
-		encoding: prevEncoding
+rax.attach();
+
+export async function request(config, prevEncoding, prevDuration) {
+  const data = {
+    ...config,
+    encoding: prevEncoding
+  };
+
+  return axios({
+    method: "POST",
+    url: "https://musenet.openai.com/sample",
+    data,
+    raxConfig: {
+		retry: 3,
+		noResponseRetries: 2,
+		retryDelay: 0,
+		backoffType: "static",
+		onRetryAttempt: err => {
+			console.warn("retrying:", err);
+		}
 	}
-
-	return axios.post("https://musenet.openai.com/sample", params)
-	.then(res => res.data)
-	.then(data => data.completions)
-	.then(tracks => tracks.map(track => parseTrack(track, prevDuration)));
+  })
+    .then(res => res.data)
+    .then(data => data.completions)
+    .then(tracks => tracks.map(track => parseTrack(track, prevDuration)));
 }
 
-function parseTrack(track, prevDuration){
-	const {totalTime: newDuration} = track;
-	const sectionDuration = newDuration - prevDuration;
+function parseTrack(track, prevDuration) {
+  const { totalTime: newDuration } = track;
+  const sectionDuration = newDuration - prevDuration;
 
-	const output = {
-		encoding: track.encoding,
-		sectionDuration,
-		duration: newDuration,
-		notes: parseNotes(track, prevDuration),
-		audio: parseAudio(track)
-	};
-	return output;
-};
-
-function transposeNotes(notes, subtract){
-	return notes.map(note => ({...note, time_on: note.time_on - subtract})).filter(note => note.time_on >= 0);
+  const output = {
+    encoding: track.encoding,
+    sectionDuration,
+    duration: newDuration,
+    notes: parseNotes(track, prevDuration),
+    audio: parseAudio(track)
+  };
+  return output;
 }
 
-function parseNotes({tracks}, prevDuration){
-	const notesPerInstrument = {};
-	instruments.forEach(instrument => notesPerInstrument[instrument] = []);
-	tracks.forEach(({instrument, notes}) => notesPerInstrument[instrument] = transposeNotes(notes, prevDuration));
-	return notesPerInstrument;
-};
+function transposeNotes(notes, subtract) {
+  return notes
+    .map(note => ({ ...note, time_on: note.time_on - subtract }))
+    .filter(note => note.time_on >= 0);
+}
 
-function parseAudio({audioFile}){
-	const trimmed = audioFile.substring(2, audioFile.length - 1);
-	return "data:audio/mp3;base64," + trimmed;
-};
+function parseNotes({ tracks }, prevDuration) {
+  const notesPerInstrument = {};
+  instruments.forEach(instrument => (notesPerInstrument[instrument] = []));
+  tracks.forEach(
+    ({ instrument, notes }) =>
+      (notesPerInstrument[instrument] = transposeNotes(notes, prevDuration))
+  );
+  return notesPerInstrument;
+}
+
+function parseAudio({ audioFile }) {
+  const trimmed = audioFile.substring(2, audioFile.length - 1);
+  return "data:audio/mp3;base64," + trimmed;
+}
